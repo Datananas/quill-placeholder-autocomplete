1f2847099c470b37f7d88b1da98dbe1df993a268
Romain SIMON <contact@romainsimon.net>
2018-09-20 10:48:51 +0200

    Fix: autocompleting from first character

--- src/autocomplete-module.js
+++ src/autocomplete-module.js
@@ -222,7 +222,7 @@ export default (Quill) => {
       this.originalQuery = this.quill.getText(this.hashIndex + 1, sel - this.hashIndex - 1);
       this.query = this.originalQuery.toLowerCase();
       // handle promise fetching custom placeholders
-      if (this.fetchPlaceholders && this.query.length && this.query.length > 4) {
+      if (this.fetchPlaceholders) {
         this.handleAsyncFetching(placeholders, labels, fs)
           .then(this.handleUpdateEnd.bind(this));
         return;
